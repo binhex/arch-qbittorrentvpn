5bc258e586539044a94fa14f10bc27181f5860ff
binhex <megalith01@gmail.com>
2019-01-16 13:38:39 +0000

    remove ref to aor.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -38,15 +38,6 @@ if [[ ! -z "${pacman_packages}" ]]; then
 	pacman -S --needed $pacman_packages --noconfirm
 fi
 
-# aor packages
-####
-
-# define arch official repo (aor) packages
-aor_packages=""
-
-# call aor script (arch official repo)
-source /root/aor.sh
-
 # aur packages
 ####
 
